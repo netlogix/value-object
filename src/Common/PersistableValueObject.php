c463fb62236076d52e3c2831d5128085c3a4edf1
Tim Weisenberger <tim.weisenberger@netlogix.de>
2025-06-23 14:31:49 +0200

    fix: Ensure compatibility with Symfony v7.3 and improve value object discovery

--- src/Common/PersistableValueObject.php
+++ src/Common/PersistableValueObject.php
@@ -7,10 +7,8 @@ namespace Netlogix\ValueObject\Common;
 use Doctrine\DBAL\ParameterType;
 use Netlogix\ValueObject\DependencyInjection\PersistableValueObjectPass;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
-use Symfony\Component\DependencyInjection\Attribute\Exclude;
 
-#[Exclude]
-#[Autoconfigure(tags: [PersistableValueObjectPass::VALUE_OBJECT_TAG], shared: false)]
+#[Autoconfigure(tags: ['container.excluded', PersistableValueObjectPass::VALUE_OBJECT_TAG], shared: false)]
 interface PersistableValueObject extends ValueObject
 {
     public static function fromRawValue($value): static;
